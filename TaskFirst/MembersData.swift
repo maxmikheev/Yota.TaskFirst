c0e68de365e40ca1fbb4d1fefa8391f62e512690
Maksim Mikheev <mmikheev@yotateam.com>
2021-11-17 01:34:16 +0300

    Создал приложение, добавил UITableViewController. Выводит из массива данные в таблицу

---
+++ TaskFirst/MembersData.swift
@@ -0,0 +1,10 @@
+//
+//  MembersData.swift
+//  TaskFirst
+//
+//  Created by Максим Михеев on 17.11.2021.
+//
+
+import Foundation
+
+var teamMembers: [String] = ["Максим Линьков","Женя Михайлин","Саша Осипец","Софья Симашкевич"]
