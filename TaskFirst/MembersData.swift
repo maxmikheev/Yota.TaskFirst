16c01e9e814a8d94a314fdc434e1ef6df033cfed
Maksim Mikheev <mmikheev@yotateam.com>
2021-11-17 20:33:07 +0300

    Добавил второй массив для специализации, добавил логику в tableView для вывода специализации в подзаголовок ячейки

--- TaskFirst/MembersData.swift
+++ TaskFirst/MembersData.swift
@@ -5,6 +5,36 @@
 //  Created by Максим Михеев on 17.11.2021.
 //
 
-import Foundation
+var teamMembers: [String] = ["Максим Линьков","Максим Новиков","Максим Михеев"]
+var specialization: [String] = ["ios", "android", "qa"]
 
-var teamMembers: [String] = ["Максим Линьков","Женя Михайлин","Саша Осипец","Софья Симашкевич"]
+
+/*
+enum Specialization {
+    case ios
+    case android
+    case qa
+}
+
+ class TeamMember {
+     var name: [String] = teamMembers
+     var specialization: Specialization
+     
+     init() {
+         self.name
+         self.specialization
+     }
+ }
+ 
+struct MembersData {
+    let teamMember: [String] = teamMembers
+    let spec: [String] = specialization
+}
+
+
+func returnMembersData() -> [MembersData] {
+    [teamMembers]
+    }
+
+}
+*/
