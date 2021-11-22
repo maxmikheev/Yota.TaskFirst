17ca9c3dc4a7fc2817c9089bfde522dffe94fdce
Maksim Mikheev <mmikheev@yotateam.com>
2021-11-23 02:06:00 +0300

    Убрал использование массивов и цикла из функции tableView

--- TaskFirst/TableViewController.swift
+++ TaskFirst/TableViewController.swift
@@ -29,50 +29,15 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell_id", for: indexPath)
-        /*
-        var cellData = cell.defaultContentConfiguration()
-        cell.textLabel?.text = teamMembers[indexPath.row]
-        cellData.text = teamMembers[indexPath.row]
-        cellData.secondaryText = specialization[indexPath.row]
-        
-        let members = teamMembers[indexPath.row]
-        let spec = specialization[indexPath.row]
-        var cellData = cell.defaultContentConfiguration()
-        
-        cellData.text = teamMembers
-        cellData.secondaryText = specialization
-        
-        let member = teamMembers[indexPath.row]
-        let spec = specialization[indexPath.row]
-         */
-        
-        //var memberData = cell.defaultContentConfiguration()
-        //memberData.text = teamMembers[indexPath.row]
-        //memberData.secondaryText = specialization[indexPath.row]
-        //memberData.text = secondTeamMembers[indexPath.row]
-        /*
-        for (member, spec) in secondTeamMembers {
-            var teamMember = member
-            var specialisation = spec
-        }
-        */
         
         var memberData = cell.defaultContentConfiguration()
         cell.contentConfiguration = memberData
         
-        var member: [String] = []
-        var spec: [String] = []
+        let teamMembers = Array(membersTeam.keys)
+        let specTeam = Array(membersTeam.values)
         
-        for (key, value) in membersTeam {
-            member.append(key)
-            spec.append(value)
-        }
-        
-        let members = member[indexPath.row]
-        let specialization = spec[indexPath.row]
-        
-        memberData.text = members
-        memberData.secondaryText = specialization
+        memberData.text = teamMembers[indexPath.row]
+        memberData.secondaryText = specTeam[indexPath.row]
         
         cell.contentConfiguration = memberData
         
