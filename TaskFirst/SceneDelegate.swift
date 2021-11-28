41a4724d3a501b1362e165d3b459362393b1ee69
Maksim Mikheev <mmikheev@yotateam.com>
2021-11-28 22:28:29 +0300

    Удалил связи сториборда, добавил в sceneDelegate инициализацию вью контроллера

--- TaskFirst/SceneDelegate.swift
+++ TaskFirst/SceneDelegate.swift
@@ -13,24 +13,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        let viewController = TeamMembersViewController()
+        let projectNavigationController = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = projectNavigationController
+        
+        self.window = window
+        window.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
-
 
 }
 
